c16a7edd47dad67a2b8b83d7b85988ed20dc6bc1
xulei12 <xulei12@baidu.com>
2016-12-21 17:21:47 +0800

    增加hadoop周期任务
    Change-Id: I718d90c288200a8a105c5fd6d313e4ba0446e2c1

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/env bash
+
 DATE=$(date +%Y%m%d -d "-1 days")
-python main.py midpage_analysis ${DATE}
+python main.py midpage_analysis ${DATE} & 1>>log/midpage_period.log 2>&1
+python main.py midpage_analysis_mapred ${DATE} & 1>>log/midpage_mapred_period.log 2>&1
